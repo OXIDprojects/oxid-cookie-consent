7fb3b943cfabeafe6fee6bf42fbd03b429b88079
kermie <info@marco-steinhaeuser.de>
2019-12-12 14:48:49 +0100

    some lang changes

--- views/admin/de/cookiemanager_lang.php
+++ views/admin/de/cookiemanager_lang.php
@@ -5,9 +5,9 @@ use OxidEsales\Eshop\Core\Registry;
 $sLangName = 'Deutsch';
 
 $aLang = array(
-    'SHOP_MODULE_GROUP_cookiemanager_settings' => 'CookieManager Einstellungen',
-    'SHOP_MODULE_GROUP_tarteaucitron_settings' => 'TarteAuCitron Einstellungen',
-    'SHOP_MODULE_GROUP_klaro_settings' => 'Klaro Einstellungen',
+    'SHOP_MODULE_GROUP_cookiemanager_settings' => 'Cookie-Manager-Einstellungen',
+    'SHOP_MODULE_GROUP_tarteaucitron_settings' => 'TarteAuCitron-Einstellungen',
+    'SHOP_MODULE_GROUP_klaro_settings' => 'Klaro-Einstellungen',
     'SHOP_MODULE_cmSettings' => 'Consent Cookie Manager',
     'SHOP_MODULE_cmSettings_tarteaucitron' => 'TarteAuCitron',
     'SHOP_MODULE_cmSettings_klaro' => 'Klaro',
@@ -17,13 +17,13 @@ $aLang = array(
     'SHOP_MODULE_tacOrientation' => 'Ausrichtung',
     'SHOP_MODULE_tacShowAlertSmall' => 'Banner',
     'SHOP_MODULE_tacCookiesList' => 'Cookieliste',
-    'SHOP_MODULE_tacAdblocker' => 'Adblocker Warnung',
-    'SHOP_MODULE_tacAcceptAllCta' => 'Alle annehmen Button',
+    'SHOP_MODULE_tacAdblocker' => 'Adblocker-Warnung',
+    'SHOP_MODULE_tacAcceptAllCta' => 'Button: "Alle annehmen"',
     'SHOP_MODULE_tacHighPrivacy' => 'Highprivacy',
     'SHOP_MODULE_tacHandleBrowserDNTRequest' => 'DNT Request',
     'SHOP_MODULE_tacRemoveCredit' => 'Credit-Link entfernen',
     'SHOP_MODULE_tacMoreInfoLink' => 'Mehr-anzeigen-Link',
-    'SHOP_MODULE_klaCookieExpiryDate' => 'Cookie Ablaufdatum',
+    'SHOP_MODULE_klaCookieExpiryDate' => 'Ablaufdatum des Cookies',
     'SHOP_MODULE_klaMustConsent' => 'Zustimmung erforderlich'
 
 
